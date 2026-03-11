3caf6fe24e4af95a3c4e4656e187848cef637d14
markuu <delgadomarkdaniel24@gmail.com>
2026-03-11 13:33:56 +0800

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -4,10 +4,12 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
   cart =[{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
+    deliveryOptionId: '1'
   },
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
+    deliveryOptionId: '2'
   }];
   
 };
@@ -31,7 +33,13 @@ export function addToCart (productId){
   );
   const quantity = Number(quantitySelector.value);
 
-  (matchingItem) ? matchingItem.quantity += quantity : cart.push({productId,quantity});
+  (matchingItem) ? 
+  matchingItem.quantity += quantity 
+  : cart.push(
+    {productId,
+    quantity, 
+    deliveryOptionId: '1'}
+  );
 
   saveToStorage();
 };
