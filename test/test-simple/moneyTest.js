6b7a8e11350003208c60e04646ceeb3e5e51e76a
markuu <delgadomarkdaniel24@gmail.com>
2026-03-17 18:40:18 +0800

    Rename tests-jasmine to test

--- test/test-simple/moneyTest.js
+++ test/test-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js'
+import {formatCurrency} from '../../scripts/utils/money.js'
 
 console.log('Test Suite: formatCurrency');
 (formatCurrency(2000.4) === '20.00') ?
