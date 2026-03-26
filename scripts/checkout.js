1ecc90ea1014a2fb33b35ae39da399e05a0a83de
markuu <delgadomarkdaniel24@gmail.com>
2026-03-26 11:19:40 +0800

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,13 +1,63 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import {checkoutHeader} from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import {getProduct, loadProducts} from "../data/products.js";
+import {loadCarts} from "../data/cart.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js'
 
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve('value1');
+    })
+  }
+  
+),
+  new Promise((resolve)=>{
+    loadCarts(()=>{
+      resolve();
+    })
+  })
+
+]).then((value)=>{
   checkoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+// new Promise((resolve)=>{
+//     loadProducts(()=>{
+//       resolve('value1');
+//     })
+//   }
+  
+// ).then((value)=>{
+//   console.log(value)
+
+//     return new Promise((resolve)=>{
+//       loadCarts(()=>{
+//         resolve();
+//       })
+//     })
+//   }
+
+// ).then(() =>{
+//     checkoutHeader();
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   }
+// );
+
+// callback for the loadProducts, it is the same as the new Promise or Promise
+
+// loadProducts(()=>{
+//   loadCarts(()=>{
+
+//     checkoutHeader();
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+ 
+// });
 
